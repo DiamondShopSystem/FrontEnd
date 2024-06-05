988799f9bd8b5ba06d32114e4be0efcfdc5e696c
nghiahoanglamfer <nghiaduong121@gmail.com>
2024-06-05 09:25:58 +0700

    user-them-san-pham-noi-bat/moi

---
+++ src/components/client/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PopularProducts from './PopularProducts'
+import NewProducts from './NewProducts'
+
+const Home = () => {
+    return (
+        <>
+            <PopularProducts />
+            <NewProducts />
+        </>
+    )
+}
+
+export default Home
