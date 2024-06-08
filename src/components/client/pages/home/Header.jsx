1a7d0f92096bbeb43ffd6a26eae175585d4ffce8
Lan Nhi <155543658+LanNhi2102@users.noreply.github.com>
2024-06-08 14:10:29 +0700

    up

--- src/components/client/pages/home/Header.jsx
+++ src/components/client/pages/home/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../../styles/Header.css';
+import './Header.css';
 
 const Header = () => {
     const [dropdownVisible, setDropdownVisible] = useState({
@@ -27,7 +27,7 @@ const Header = () => {
             <div className="header__center">
                 <img src="/logo.png" alt="Thế Giới Kim Cương" className="header__logo-image" />
                 <div className="header__logo-text">
-                    <span className="header__logo-title">TRÙM KIM CƯƠNG</span>
+                    <span className="header__logo-title">THẾ GIỚI KIM CƯƠNG</span>
                 </div>
             </div>
             <div className="header__bottom">
@@ -55,11 +55,11 @@ const Header = () => {
                             Trang Sức Kim Cương
                             {dropdownVisible["Trang Sức Kim Cương"] && (
                                 <ul className="dropdown__content">
-                                    <li className="dropdown__item"><img src="/icons/ring.png" alt="Nhẫn" /> Nhẫn</li>
-                                    <li className="dropdown__item"><img src="/icons/earrings.png" alt="Hoa Tai" /> Hoa Tai</li>
-                                    <li className="dropdown__item"><img src="/icons/necklace.png" alt="Mặt Dây" /> Mặt Dây</li>
-                                    <li className="dropdown__item"><img src="/icons/bracelet.png" alt="Lắc Tay" /> Lắc Tay</li>
-                                    <li className="dropdown__item"><img src="/icons/bangle.png" alt="Vòng Tay" /> Vòng Tay</li>
+                                    <li className="dropdown__item"><img src="//file.hstatic.net/1000381168/file/ic-6_a8cf702ba6b646a4bf9c7ef2e69ea3cd.png" alt="icon level 3" /> Nhẫn</li>
+                                    <li className="dropdown__item"><img src="//file.hstatic.net/1000381168/file/ic-11_0b2efe255a95499fa470d3177eec91ab.png" alt="icon level 3" /> Hoa Tai</li>
+                                    <li className="dropdown__item"><img src="https://file.hstatic.net/1000381168/file/mat-day-chuyen_e183174a263f447699d806d8ecc3cf25.png" alt="icon level 3" /> Mặt Dây</li>
+                                    <li className="dropdown__item"><img src="https://file.hstatic.net/1000381168/file/day-lac-tay_38c9df8f6b3241219b648693d7231d2a.png" alt="icon level 3" /> Lắc Tay</li>
+                                    <li className="dropdown__item"><img src="https://file.hstatic.net/1000381168/file/vong-tay_954bb2c1919c43d69bf24c97ff75fc0a.png" alt="icon level 3" /> Vòng Tay</li>
                                 </ul>
                             )}
                         </li>
