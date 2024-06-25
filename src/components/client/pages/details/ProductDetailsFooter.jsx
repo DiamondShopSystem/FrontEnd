2900eb9a286e0c07fcb73217766699742321c318
Lan Nhi <155543658+LanNhi2102@users.noreply.github.com>
2024-06-26 01:40:12 +0700

    up product details

---
+++ src/components/client/pages/details/ProductDetailsFooter.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../../styles/ProductDetails.css';
+
+const ProductDetailsFooter = () => {
+    return (
+        <div className="details-footer">
+            <div className="footer-section">
+                <div className="commitment">
+                    <h4>
+                        <img src="https://theme.hstatic.net/1000381168/1000941305/14/titlebottomuudaiicon1.png?v=604" alt="Commitment Icon" /> CAM KẾT CHẤT LƯỢNG
+                    </h4>
+                    <ul>
+                        <li>Trang sức/ Ổ trang sức 100% chuẩn xác về hàm lượng và trọng lượng vàng.</li>
+                        <li>Kim cương tự nhiên được nhập khẩu chính ngạch 100% và có giấy kiểm định GIA/ IGI uy tín thế giới và có giá trị toàn cầu. Đây đủ máy móc, trang thiết bị hiện đại để kiểm tra chất lượng và soi mã số cạnh kim cương.</li>
+                        <li>Đồng hồ chính hãng các thương hiệu: Versace, Mathey-Tissot, Pierre Cardin bảo hành chính hãng.</li>
+                        <li>Trang sức vàng ta 24k (999.9) 100% chuẩn xác về hàm lượng và trọng lượng vàng.</li>
+                        <li>Mọi sản phẩm do Thế Giới Kim Cương bán ra đều có hóa đơn tài chính hợp lệ.</li>
+                    </ul>
+                </div>
+                <div className="guide">
+                    <h4>
+                        <img src="https://theme.hstatic.net/1000381168/1000941305/14/titlebottomuudaiicon2.png?v=604" alt="Guide Icon" /> HƯỚNG DẪN MUA HÀNG ONLINE
+                    </h4>
+                    <ul>
+                        <li>Quý Khách vào trang sản phẩm để xem các sản phẩm được đăng tải trên Website.</li>
+                        <li>Thêm vào giỏ hàng.</li>
+                        <li>Kiểm tra thông tin đơn hàng và đặt hàng.</li>
+                        <li>Kiểm tra và xác nhận đơn hàng.</li>
+                        <li>Đơn hàng thành công sẽ được gửi về email & sms của quý khách, đồng thời nhân viên sẽ liên hệ lại với Quý Khách xác nhận thời gian giao hàng. Xem chi tiết.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductDetailsFooter;
