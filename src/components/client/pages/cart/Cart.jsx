acfc9a4c425258655959f745b0e3e17803a8cb54
nguyenphuoc1509 <nguyenphuoc150903@gmail.com>
2024-07-13 13:41:21 +0700

    up code cart

--- src/components/client/pages/cart/Cart.jsx
+++ src/components/client/pages/cart/Cart.jsx
@@ -3,12 +3,11 @@ import '../../styles/Cart.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
 
 const Cart = () => {
     const [cart, setCart] = useState([]);
     const [totalPrice, setTotalPrice] = useState([]);
-    const [size, setSize] = useState();
+
     useEffect(() => {
         fetchData();
     }, []);
@@ -21,8 +20,6 @@ const Cart = () => {
                     ...product,
                     productInfo: product.productInfo[0],
                 }));
-                setSize(response.data.cart.products.size);
-                console.log(response.data.cart.products.size);
                 setCart(products);
                 setTotalPrice(response.data.cart.totalPrice);
             }
@@ -31,109 +28,112 @@ const Cart = () => {
         }
     };
 
-    const handleDecrement = (card_id) => {
-        console.log(cart);
-        setCart(cart =>
-            cart.map((item) => card_id === item.id ? { ...item, quantity : item.quantity - 1} : item)
-        )
+    // const updateCartQuantity = async (productId, quantity) => {
+    //     try {
+    //         await axios.patch(`/cart/update/${productId}`, { quantity });
+    //         fetchData();
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // };
+    const updateCartQuantity = async (productId, quantity, e) => {
+        e.preventDefault();
+        await axios.patch(`/cart/update/${productId}`, quantity)
+            .then((result) => {
+                fetchData();
+            })
+            .catch((error) => { })
     }
-    const handleIncrement = (card_id) => {
-        setCart(cart =>
-            cart.map((item) => card_id === item.id ? { ...item, quantity: item.quantity + 1} : item)
-        )
-    }
+
+    const handleQuantityChange = (index, delta) => {
+        const newCartData = [...cart];
+        newCartData[index].quantity += delta;
+        if (newCartData[index].quantity < 1) {
+            newCartData[index].quantity = 1;
+        }
+        setCart(newCartData);
+        
+        updateCartQuantity(newCartData[index].product_id, newCartData[index].quantity);
+    };
 
     const handleInputChange = (index, value) => {
         const newCartData = [...cart];
         newCartData[index].quantity = value < 1 ? 1 : value;
         setCart(newCartData);
+        updateCartQuantity(newCartData[index].product_id, newCartData[index].quantity);
     };
 
-    const handleDelete = async (id) => {
-        axios.delete(`/cart/delete/${id}`)
-            .then(response => {
-                console.log(response);
-                fetchData();
-                toast.success('Xóa thành công')
-
-            })
-            .catch(error => {
-                console.error(error);
-                toast.error('Xóa không thành công');
-            });
+    const handleDelete = (index) => {
+        const newCartData = cart.filter((_, i) => i !== index);
+        setCart(newCartData);
     };
 
     return (
-        <>
-            <ToastContainer />
-            <div className="cart__container">
-                <div className="cart__header">
-                    <h1>Giỏ hàng của tôi</h1>
-                </div>
-                <div className="cart__table">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>STT</th>
-                                <th>Sản Phẩm</th>
-                                <th>Số Lượng</th>
-                                <th>Đơn Giá</th>
-                                <th>Thành Tiền</th>
+        <div className="cart__container">
+            <div className="cart__header">
+                <h1>Giỏ hàng của tôi</h1>
+            </div>
+            <div className="cart__table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>STT</th>
+                            <th>Sản Phẩm</th>
+                            <th>Số Lượng</th>
+                            <th>Đơn Giá</th>
+                            <th>Thành Tiền</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cart.map((item, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>
+                                    <div className="cart__product-info">
+                                        <Link to='/'>
+                                            <img src={item.productInfo.thumbnail} alt="Ảnh" className="cart__product-image" />
+                                        </Link>
+                                        <div>
+                                            <div>{item.productInfo.title}</div>
+                                            <div>Ni (size): {item.size}</div> {/* Hiển thị size được chọn */}
+                                        </div>
+                                        <div className="cart__trash-icon" onClick={() => handleDelete(index)}>
+                                            <i className="fas fa-trash"></i>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className="cart__quantity-control">
+                                        <button onClick={() => handleQuantityChange(index, -1)}>-</button>
+                                        <input
+                                            type="number"
+                                            value={item.quantity}
+                                            onChange={(e) => handleInputChange(index, parseInt(e.target.value))}
+                                        />
+                                        <button onClick={() => handleQuantityChange(index, 1)}>+</button>
+                                    </div>
+                                </td>
+                                <td>{item.productInfo.price?.toLocaleString()}đ</td>
+                                <td><b>{(item.productInfo.price * item.quantity)?.toLocaleString()}đ</b></td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            {cart.map((item, index) => (
-                                <tr key={index}>
-                                    <td>{index + 1}</td>
-                                    <td>
-                                        <div className="cart__product-info">
-                                            <Link to='/'>
-                                                <img src={item.productInfo.thumbnail} alt="Ảnh" className="cart__product-image" />
-                                            </Link>
-                                            <div>
-                                                <div>{item.productInfo.title}</div>
-                                                <div>Ni (size): {item.size}</div> {/* Hiển thị size được chọn */}
-                                            </div>
-                                            <div className="cart__trash-icon" onClick={() => handleDelete(item.product_id)}>
-                                                <i className="fas fa-trash"></i>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div className="cart__quantity-control">
-                                            <button onClick={() => handleDecrement(item.product_id)}>-</button>
-                                            <input
-                                                type="number"
-                                                value={item.quantity}
-                                                onChange={(e) => handleInputChange(index, parseInt(e.target.value))}
-                                            />
-                                            <button onClick={() => handleIncrement(item.product_id)}>+</button>
-                                        </div>
-                                    </td>
-                                    <td>{item.productInfo.price?.toLocaleString()}đ</td>
-                                    <td><b>{(item.productInfo.price * item.quantity)?.toLocaleString()}đ</b></td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
 
-                <div className="cart__footer">
-                    <div className="cart__total-amount">
-                        <div className="total">
-                            <span>TỔNG TIỀN (tạm tính): </span>
-                            {/* <span style={{ marginLeft: '10px', fontSize: '25px' }}>{calculateTotal()?.toLocaleString()}đ</span> */}
-
-                            <span style={{ marginLeft: '10px', fontSize: '25px' }}>{totalPrice?.toLocaleString()}đ</span>
-                        </div>
-                        <div className="checkout">
-                            <button className="cart__checkout-btn">THANH TOÁN</button>
-                        </div>
+            <div className="cart__footer">
+                <div className="cart__total-amount">
+                    <div className="total">
+                        <span>TỔNG TIỀN (tạm tính): </span>
+                        
+                        <span style={{ marginLeft: '10px', fontSize: '25px' }}>{totalPrice?.toLocaleString()}đ</span>
+                    </div>
+                    <div className="checkout">
+                        <button className="cart__checkout-btn">THANH TOÁN</button>
                     </div>
                 </div>
             </div>
-        </>
-
+        </div>
     );
 }
 
